7217130a660b0598840ca9c9f3b2a10fe5adf243
ferdi.tatlisu <ferdi.tatlisu@trendyol.com>
2023-06-13 09:50:43 +0300

    KB-1 slack url added to config

--- nginx_entrypoint.sh
+++ nginx_entrypoint.sh
@@ -2,10 +2,13 @@
 set -e
 
 env_content='KB_API'="$KB_API"
+env_slack='SLACK_URL'="$SLACK_URL"
 printf "$env_content" > /usr/share/nginx/html/.env
+printf "$env_slack" > /usr/share/nginx/html/.env2
 
 echo "KB_ENVIRONMENTS = {" > /usr/share/nginx/html/env-config.js
 awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' /usr/share/nginx/html/.env >> /usr/share/nginx/html/env-config.js
+awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' /usr/share/nginx/html/.env2 >> /usr/share/nginx/html/env-config.js
 echo "}" >> /usr/share/nginx/html/env-config.js
 
 
